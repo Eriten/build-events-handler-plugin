52d8f46d0cd1c1f2983ee27c646c2b10fecbb851
Eric Tam <etam@sugarcrm.com>
2016-05-23 23:59:19 -0700

    Started project with Maven and gitignore

---
+++ src/main/java/org/jenkinsci/plugins/buildEventsHandler/AbstractBuildEventsWrapper.java
@@ -0,0 +1,9 @@
+package org.jenkinsci.plugins.buildEventsHandler;
+
+import hudson.tasks.BuildWrapper;
+
+/**
+ * Created by etam on 5/23/16.
+ */
+public class AbstractBuildEventsWrapper extends BuildWrapper {
+}
---
+++ src/main/java/org/jenkinsci/plugins/buildEventsHandler/BuildEventsListener.java
@@ -0,0 +1,10 @@
+package org.jenkinsci.plugins.buildEventsHandler;
+
+import hudson.model.AbstractBuild;
+import hudson.model.listeners.RunListener;
+
+/**
+ * Created by etam on 5/23/16.
+ */
+public class BuildEventsListener extends RunListener<AbstractBuild<?, ?>> {
+}
